c82b20c7347adc05e6de9d253443471d321f4aa6
Ryan Mossor <ryan.mossor@gmail.com>
2023-10-11 18:45:17 -0500

    Day 1

---
+++ day1/Program.cs
@@ -0,0 +1,25 @@
+﻿var counter = 0;
+var totals = new List<int>();
+
+try {
+    using (StreamReader reader = new StreamReader("input.txt")) {
+        string line;
+        line = reader.ReadLine();
+
+        while (line != null) {
+            if (string.IsNullOrWhiteSpace(line)) {
+                totals.Add(counter);
+                counter = 0;
+            } else {
+                counter += Convert.ToInt32(line);
+            }
+                
+            line = reader.ReadLine();
+        }
+    }
+
+    var topThreeTotal = totals.OrderDescending().Take(3).Sum();
+    Console.WriteLine(topThreeTotal);
+} catch(Exception e) {
+    Console.WriteLine($"Exception: {e.Message}");
+}

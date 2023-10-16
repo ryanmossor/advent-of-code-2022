a7f27a7f4142a466d3aed9d84ffc59c9aeb53c75
Ryan Mossor <ryan.mossor@gmail.com>
2023-10-16 12:13:53 -0500

    Day 6

---
+++ day6/Program.cs
@@ -0,0 +1,27 @@
+﻿string dataBuffer;
+
+try {
+    using (StreamReader reader = new StreamReader("./input.txt"))
+        dataBuffer = reader.ReadToEnd();
+    
+    Console.WriteLine($"Packet start: {searchBufferForStart(dataBuffer, BufferMarker.StartOfPacket)}");
+    Console.WriteLine($"Message start: {searchBufferForStart(dataBuffer, BufferMarker.StartOfMessage)}");
+} catch(Exception e) {
+    Console.WriteLine(e.Message);
+}
+
+int searchBufferForStart(string buffer, BufferMarker marker) {
+    for (int i = 0; i < buffer.Length; i++) {
+        var sub = buffer.Substring(i, (int)marker);
+        if (sub.Distinct().ToArray().Count() == (int)marker)
+            return i + sub.Length;
+    }
+
+    return 0;
+}
+
+enum BufferMarker {
+    StartOfPacket = 4,
+    StartOfMessage = 14
+}
+

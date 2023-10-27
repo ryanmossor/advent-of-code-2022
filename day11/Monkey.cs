f2570ad7143cb2e6205ae2dcf70f9c652b7bb561
Ryan Mossor <ryan.mossor@gmail.com>
2023-10-26 20:40:10 -0500

    Day 11 part 2

--- day11/Monkey.cs
+++ day11/Monkey.cs
@@ -1,8 +1,8 @@
 namespace Day11 {
     public class Monkey {
         public int MonkeyId { get; private set; }
-        public Queue<int> Items { get; private set; }
-        public int WorryLevel { get; private set; }
+        public Queue<long> Items { get; private set; }
+        public long WorryLevel { get; private set; }
         public Operation MathOperation { get; private set; }
         public int OperationValue { get; private set; }
         public int Divisor { get; private set; }
@@ -10,7 +10,7 @@ namespace Day11 {
         public int FalseTarget { get; private set; }
         public long InspectionCount { get; private set; }
 
-        public Monkey(int monkeyId, Queue<int> startingItems, Operation operation, int divisor, int trueTarget, int falseTarget, int operationValue = -1) {
+        public Monkey(int monkeyId, Queue<long> startingItems, Operation operation, int divisor, int trueTarget, int falseTarget, int operationValue = -1) {
             MonkeyId = monkeyId;
             Items = startingItems;
             WorryLevel = 0;
@@ -22,12 +22,11 @@ namespace Day11 {
             InspectionCount = 0;
         }
 
-        public void TransferItem(List<Monkey> monkeys) {
+        public void TransferItem(List<Monkey> monkeys, bool allowRelief, int lcm) {
             var item = Items.Dequeue();
             WorryLevel = item;
             InspectionCount++;
-            WorryLevel = DoMath();
-            WorryLevel = SweetRelief();
+            WorryLevel = DoMath(lcm, allowRelief);
 
             var targetMonkey = WorryLevel % Divisor == 0
                 ? monkeys[TrueTarget]
@@ -36,24 +35,23 @@ namespace Day11 {
             targetMonkey.ReceiveItem(WorryLevel);
         }
 
-        public int DoMath() {
-            var val = OperationValue;
+        public long DoMath(int lcm, bool allowRelief) {
+            long val = OperationValue;
             if (OperationValue == -1)
                 val = WorryLevel;
 
+            long result;
             if (MathOperation == Operation.Add)
-                return WorryLevel + val;
+                result = WorryLevel + val;
             else
-                return WorryLevel * val;
+                result = WorryLevel * val;
+
+            if (allowRelief)
+                return (int)Math.Floor((double)(result / 3));
+            else
+                return result % lcm;
         }
 
-        public int SweetRelief() {
-            double relievedWorryLevel = WorryLevel / 3;
-            return (int)Math.Floor(relievedWorryLevel);
-        }
-
-        public void ReceiveItem(int itemWorryLevel) {
-            Items.Enqueue(itemWorryLevel);
-        }
+        public void ReceiveItem(long itemWorryLevel) => Items.Enqueue(itemWorryLevel);
     }
 }
--- day11/Program.cs
+++ day11/Program.cs
@@ -2,50 +2,63 @@
 using Day11;
 
 var input = File.ReadAllText("./input.txt");
-var monkeys = new List<Monkey>();
 var monkeyStats = input.Split("\n\n");
 
 var regex = new Regex(@"^Monkey (?<monkeyId>\d+):\n\s*Starting items: (?<startingItems>\d+(,\s\d+)*)\n\s*Operation: new = old (?<operator>[\+\-\*\/]) (?<operationValue>\d+|old)\n\s*Test: divisible by (?<divisor>\d+)\n\s*If true: throw to monkey (?<trueTarget>\d+)\n\s*If false: throw to monkey (?<falseTarget>\d+)$", RegexOptions.ECMAScript);
 
-foreach (var stat in monkeyStats) {
-    var match = regex.Match(stat);
-    var monkeyId = Convert.ToInt32(match.Groups["monkeyId"].Value);
+System.Console.WriteLine($"Part 1: {PlayKeepAway(20, allowRelief: true)}");
+System.Console.WriteLine($"Part 2: {PlayKeepAway(10_000, allowRelief: false)}");
 
-    var items = match.Groups["startingItems"].Value
-        .ToString()
-        .Split(", ")
-        .Select(i => Convert.ToInt32(i))
-        .ToList();
-    var startingQueue = new Queue<int>();
-    foreach (var item in items)
-        startingQueue.Enqueue(item);
+List<Monkey> Init() {
+    var monkeys = new List<Monkey>();
+    foreach (var stat in monkeyStats) {
+        var match = regex.Match(stat);
+        var monkeyId = Convert.ToInt32(match.Groups["monkeyId"].Value);
 
-    var op = match.Groups["operator"].Value == "+"
-        ? Operation.Add
-        : Operation.Multiply;
+        var items = match.Groups["startingItems"].Value
+            .ToString()
+            .Split(", ")
+            .Select(i => Convert.ToInt32(i))
+            .ToList();
+        var startingQueue = new Queue<long>();
+        foreach (var item in items)
+            startingQueue.Enqueue(item);
 
-    var operationValue = match.Groups["operationValue"].Value == "old"
-        ? -1
-        : Convert.ToInt32(match.Groups["operationValue"].Value);
+        var op = match.Groups["operator"].Value == "+"
+            ? Operation.Add
+            : Operation.Multiply;
 
-    var divisor = Convert.ToInt32(match.Groups["divisor"].Value);
-    var trueTarget = Convert.ToInt32(match.Groups["trueTarget"].Value);
-    var falseTarget = Convert.ToInt32(match.Groups["falseTarget"].Value);
+        var operationValue = match.Groups["operationValue"].Value == "old"
+            ? -1
+            : Convert.ToInt32(match.Groups["operationValue"].Value);
 
-    var monkey = new Monkey(monkeyId, startingQueue, op, divisor, trueTarget, falseTarget, operationValue);
-    monkeys.Add(monkey);
-}
+        var divisor = Convert.ToInt32(match.Groups["divisor"].Value);
+        var trueTarget = Convert.ToInt32(match.Groups["trueTarget"].Value);
+        var falseTarget = Convert.ToInt32(match.Groups["falseTarget"].Value);
 
-for (int i = 0; i < 20; i++) {
-    foreach (var monkey in monkeys) {
-        while (monkey.Items.Count() > 0)
-            monkey.TransferItem(monkeys);
+        var monkey = new Monkey(monkeyId, startingQueue, op, divisor, trueTarget, falseTarget, operationValue);
+        monkeys.Add(monkey);
     }
+
+    return monkeys;
 }
 
-var topTwoProduct = monkeys
-    .OrderByDescending(x => x.InspectionCount)
-    .Take(2)
-    .Select(m => m.InspectionCount)
-    .Aggregate((s, a) => s * a);
-System.Console.WriteLine(topTwoProduct);
+long PlayKeepAway(int rounds, bool allowRelief) {
+    var monkeys = Init();
+    int lcm = monkeys.Select(x => x.Divisor).Aggregate((s, a) => s * a);
+
+    for (int i = 0; i < rounds; i++) {
+        foreach (var monkey in monkeys) {
+            while (monkey.Items.Count() > 0)
+                monkey.TransferItem(monkeys, allowRelief, lcm);
+        }
+    }
+
+    var topTwoProduct = monkeys
+        .OrderByDescending(x => x.InspectionCount)
+        .Take(2)
+        .Select(m => m.InspectionCount)
+        .Aggregate((s, a) => s * a);
+
+    return topTwoProduct;
+}

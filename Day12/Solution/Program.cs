cdb84546faf6b194b9d057c5373510e6a2148dcb
Ryan Mossor <ryan.mossor@gmail.com>
2023-11-10 17:01:25 -0600

    Day 12 part 2

--- Day12/Solution/Program.cs
+++ Day12/Solution/Program.cs
@@ -3,11 +3,15 @@
 var input = File.ReadAllLines("./input.txt");
 
 int[,] heights = new int[input.Length, input[0].Length];
+var startingPositions = new List<Position>();
+
 for (int r = 0; r < input.Length; r++) {
     for (int c = 0; c < input[0].Length; c++) {
         char ch = input[r][c];
-        if (ch == 'S')
+        if (ch == 'S' || ch == 'a') {
             heights[r,c] = 0;
+            startingPositions.Add(new Position(r, c));
+        }
         else if (ch == 'E')
             heights[r,c] = 'z' - 'a';
         else
@@ -23,15 +27,19 @@ string endingLine = input.First(x => x.Contains('E'));
 int endingRow = Array.IndexOf(input, endingLine);
 int endingCol = endingLine.IndexOf('E');
 
-var startingPos = new Position(startingRow, startingCol);
-var endingPos = new Position(endingRow, endingCol);
+var distances = new List<int>();
 
-var explorer = new Explorer(new Maze(heights), startingPos);
+foreach (var startingPos in startingPositions) {
+    var endingPos = new Position(endingRow, endingCol);
+    var explorer = new Explorer(new Maze(heights), startingPos);
 
-while (explorer.ExplorationQueue.Count() > 0) {
-    Position exploring = explorer.Explore();
-    if (exploring == endingPos) {
-        Console.WriteLine(explorer.DistanceTo(exploring));
-        break;
+    while (explorer.ExplorationQueue.Count() > 0) {
+        Position exploring = explorer.Explore();
+        if (exploring == endingPos) {
+            distances.Add(explorer.DistanceTo(exploring));
+            break;
+        }
     }
 }
+
+Console.WriteLine(distances.Order().First());
